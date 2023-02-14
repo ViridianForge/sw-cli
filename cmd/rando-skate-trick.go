efa59795176a9cc285bbaae8e6523d92c4cc05ef
Wayne Manselle <wayne@viridianforge.tech>
2023-02-13 23:02:08 -0800

    Add skate tricks and resource files for later use

---
+++ cmd/rando-skate-trick.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var skateTrickPropertyMap = [][]string{
+	{"Steady", "Floating", "Rotated", "Inverted", "Greasy", "Upside", "Greyside", "Purring", "Barky", "Soggy", "Reversed", "Fuzzy", "Hanging", "Salty", "Thirsty", "Curvy", "Parallax", "Highlow", "Side", "Crunchy"},
+	{"Roll", "Puff", "Skizzle", "Nutsome", "Chafer", "Yanker", "Burner", "Monger", "Dilly", "Jortle", "Crunchy", "Thwappy", "Snappy", "Clapper", "Stoney", "Punted", "Wiggle", "Shooketh", "Pendulus", "Doppler"},
+	{"Frazz", "Burner", "Dump", "Chunt", "Breff", "Plop", "Nay", "Whizz", "Knob", "Goat", "Bruh", "Flex", "Cap", "Tope", "Smash", "Plug", "Mofo", "Seam", "Blaze", "Streak"},
+	{"Grumble", "Stunted", "Lazy", "Glitchy", "Wrinkle", "Rising", "Saucy", "Flabby", "Wumpus", "Fuddy", "Lickety", "Muffin", "Whirly", "Squeegee", "Wishy", "Diddle", "Fripper", "Wishy", "Saluting", "Blubber"},
+	{"Twist", "Pop", "Mast", "Drip", "Hole", "Melter", "Smack", "Schmooze", "Dollop", "Boo", "Daddle", "Pronk", "Hog", "Wink", "Spleen", "Fop", "Roll", "Bend", "Choke", "Dingle"},
+}
+
+func init() {
+	rootCmd.AddCommand(randoSkateTrickCmd)
+}
+
+func skateTrickProperty(row int) string {
+	rand.Seed(time.Now().UnixNano())
+	return skateTrickPropertyMap[row][rand.Intn(11)+1]
+}
+
+var randoSkateTrickCmd = &cobra.Command{
+	Use:   "rando-trick",
+	Short: "Generates a random skate trick name",
+	Long:  "Generates a skate trick name based on the table available in the standard rule book",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Your skate trick name is %s %s %s with a %s %s\n", skateTrickProperty(0), skateTrickProperty(1), skateTrickProperty(2), skateTrickProperty(3), skateTrickProperty(4))
+	},
+}
--- cmd/rando-spell.go
+++ cmd/rando-spell.go
@@ -12,16 +12,16 @@ func init() {
 	rootCmd.AddCommand(randoSpellCmd)
 }
 
-var propertyMap = [][]string{
+var randoSpellMap = [][]string{
 	{"Awesome", "Busted", "Dope", "Epic", "Gnarly", "Hyped", "Janky", "Killer", "Psyched", "Rad", "Sketchy", "Stoked"},
 	{"Animating", "Attracting", "Bewildering", "Concealing", "Consuming", "Crushing", "Duplicating", "Expanding", "Revealing", "Sealing", "Shielding", "Summoning"},
 	{"Acid", "Air", "Dust", "Earth", "Fire", "Light", "Reflecting", "Shadow", "Smoke", "Sound", "Spirit", "Water"},
 	{"Armor", "Boot", "Bread", "Bucket", "Chain", "Door", "Hammer", "Lute", "Mattress", "Tower", "Tree", "Well"},
 }
 
-func randomProperty(row int) string {
+func randoSpellProperty(row int) string {
 	rand.Seed(time.Now().UnixNano())
-	return propertyMap[row][rand.Intn(11)+1]
+	return randoSpellMap[row][rand.Intn(11)+1]
 }
 
 var randoSpellCmd = &cobra.Command{
@@ -29,6 +29,6 @@ var randoSpellCmd = &cobra.Command{
 	Short: "Generates a random rando spell",
 	Long:  "Generates a rando spell based on the table available in the standard rule book",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Your rando-spell is %s %s %s %s", randomProperty(0), randomProperty(1), randomProperty(2), randomProperty(3))
+		fmt.Printf("Your rando-spell is %s %s %s %s\n", randoSpellProperty(0), randoSpellProperty(1), randoSpellProperty(2), randoSpellProperty(3))
 	},
 }
